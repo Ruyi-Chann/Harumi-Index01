514ddfb766e2325f68c064926b996d35fb63d865
FlureTzy <75057239+Ruyi-Chann@users.noreply.github.com>
2021-02-24 16:58:48 -0800

    Update api1.php

--- api/api1.php
+++ api/api1.php
@@ -1,5 +1,5 @@
 <?php
 
-echo 'Your API Goes to api/your-api-name.php, Please read instructions how to assign badges on index.php!';
+echo 'Your API Goes to api/your-api-name.php, Please read instructions how to assign badges on index.php!<br>';
 
 ?>
